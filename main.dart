50f2f5c086d7c01e0d2ae3deacf03d2fbea6f8d2
amitsarker95 <94947088+amitsarker95@users.noreply.github.com>
2021-11-29 04:27:12 +0000

    Initial commit

---
+++ main.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+
+void main() {
+	stdout.write("What's your name? ");
+  String name = stdin.readLineSync()!;
+
+  print("Hi, $name! What is your age?");
+  int age = int.parse(stdin.readLineSync()!);
+  int yearsToHunderd = 100 - age;
+  print("$name, you have $yearsToHunderd years to be 100");
+}
