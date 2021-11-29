afb72594da4ddd75d6ec9202b52c22ca8897da26
amitsarker95 <94947088+amitsarker95@users.noreply.github.com>
2021-11-29 05:38:57 +0000

    example 14

---
+++ example-14.dart
@@ -0,0 +1,14 @@
+import 'dart:io';
+
+void main() {
+  stdout.write("Please give a sentence: ");
+  String sentence = stdin.readLineSync()!;
+
+  reverseSentence(sentence);
+}
+
+void reverseSentence(String sentence) {
+  
+  String a = sentence.split(" ").reversed.toList().join(" ");
+  print(a);
+}
