3c6db1df114dcbb1a87399dcd3fa39e8fc815358
amitsarker95 <94947088+amitsarker95@users.noreply.github.com>
2021-11-29 05:59:58 +0000

    example 19

---
+++ example-19.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+void main() {
+ 
+  List<List<String>> initialBoard =
+      List.generate(3, (_) => List.generate(3, (_) => ' '));
+  drawBoard(initialBoard, 2);
+}
+
+void drawBoard(List<List<String>> board, int currentUser) {
+ 
+  var move;
+  currentUser == 1 ? move = 'X' : move = 'O';
+
+  stdout.write("Please choose a coordinate: ");
+  List choice = stdin.readLineSync()!.split(" ");
+  board[int.parse(choice[0])][int.parse(choice[1])] = move;
+  print(board);
+}
