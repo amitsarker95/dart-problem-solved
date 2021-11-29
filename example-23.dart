b49fe1878d6c278e4fc56886aef4ff4bef015e18
amitsarker95 <94947088+amitsarker95@users.noreply.github.com>
2021-11-29 06:46:14 +0000

    example 23

---
+++ example-23.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  String word = randomWord("sowpods.txt");
+  print(word);
+}
+
+String randomWord(String txt) {
+ 
+  final random = Random();
+  var file = File(txt);
+  List<String> wordList = file.readAsLinesSync();
+  String word = wordList[random.nextInt(wordList.length)];
+  return word;
+}
