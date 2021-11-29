24b1479cc7a4ea2af4a5f748e1355cfd525fa947
amitsarker95 <94947088+amitsarker95@users.noreply.github.com>
2021-11-29 05:52:23 +0000

    example 16

---
+++ example-16.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+import 'dart:math';
+
+void main() {
+  
+  final random = Random();
+  String randomNumber = (1000 + random.nextInt(9999 - 1000)).toString();
+  print(randomNumber);
+
+  stdout.write("Welcome to Cows and Bulls\nType 'exit' to stop the game\n");
+
+  int attempts = 0;
+
+
+  while (true) {
+    int cows = 0;
+    int bulls = 0;
+    attempts += 1;
+
+    stdout.write("\nPlease choose a four digit number: ");
+    String chosenNumber = stdin.readLineSync()!;
+
+    
+    if (chosenNumber == randomNumber) {
+      print("Bullseye! You took $attempts attempts");
+      break;
+    } else if (chosenNumber == "exit") {
+      print("Bye bye!");
+      break;
+    } else if (chosenNumber.length != randomNumber.length) {
+      print("Incorrect number. Make sure to give 4 digit number");
+      continue;
+    }
+
+    
+    for (var i = 0; i < randomNumber.length; i++) {
+      if (chosenNumber[i] == randomNumber[i]) {
+        cows += 1;
+      } else if (randomNumber.contains(chosenNumber[i])) {
+        bulls += 1;
+      }
+    }
+    print("\nAttempts: $attempts \nCows: $cows, Bulls: $bulls");
+  }
+}
